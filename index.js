3804fc13921c6c1f3073319e8d3ce65c774fb3c0
Dr-Appukuttan <73917119+Dr-Appukuttan@users.noreply.github.com>
2020-12-15 18:56:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
  */
 client.on("ready", () => {
    function randomStatus() {
- let status = ["Android Discord", "MUSIC", "50,000 guilds", "ad!help | ad!play"]
+ let status = ["Bennet Gaming", ".help", "YOU", "Over Server"]
 let rstatus = Math.floor(Math.random() * status.length);
 
 client.user.setActivity(status[rstatus], {type: "WATCHING" });
